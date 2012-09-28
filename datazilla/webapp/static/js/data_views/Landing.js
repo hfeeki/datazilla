43041d39d74ea15870c4a3c7924b81d8e2e3bf99
Cameron Dawson <cdawson@mozilla.com>
2012-09-28 10:43:56 -0700

    minor UI change and indentation fix

--- datazilla/webapp/static/js/data_views/Landing.js
+++ datazilla/webapp/static/js/data_views/Landing.js
@@ -2,21 +2,21 @@ function displayTable(project) {
     $('#summary_project_name').html(project);
 
     $.ajax({
-               url: "/" + project + "/refdata/perftest/runs_by_branch?days_ago=30&show_test_runs=True"
-           }).done(function(data) {
-                       var table = $('#templates .branch_results').clone();
+        url: "/" + project + "/refdata/perftest/runs_by_branch?days_ago=30&show_test_runs=True"
+        }).done(function(data) {
+            var table = $('#templates .branch_results').clone();
 
-                       for (var branch in data) {
-                           var table_aaData = [];
-                           for (var i = 0; i < data[branch]["test_runs"].length; i++) {
-                               var run = data[branch]["test_runs"][i];
-                               table_aaData.push([run["date_run"], run["product"], run["version"], run["revision"]]);
-                           }
+            for (var branch in data) {
+                var table_aaData = [];
+                for (var i = 0; i < data[branch]["test_runs"].length; i++) {
+                    var run = data[branch]["test_runs"][i];
+                    table_aaData.push([run["date_run"], run["product"], run["version"], run["revision"]]);
+                }
 
-                           table.find('#branch_name').html(branch);
-                           $('#results .data').html(table);
+                table.find('#branch_name').html(branch);
+                $('#results .data').html(table);
 
-                           this.resultsTable = table.find('table').dataTable({
+                this.resultsTable = table.find('table').dataTable({
                                                                                  aaData: table_aaData,
                                                                                  aoColumns: [
                                                                                      { sTitle: 'date run' },
@@ -31,6 +31,6 @@ function displayTable(project) {
                                                                                  bAutoWidth: true
 
                                                                              });
-                       }
-                   })
+            }
+        })
 }
